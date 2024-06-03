1b26a0169e749f93a904d26d17f79d9b46cc4818
poonamsaxena123 <saxena2111poonam@gmail.com>
2024-06-03 18:43:10 +0530

    update_list Task

--- list_task.py
+++ list_task.py
@@ -1,3 +1,4 @@
+import copy
 # list methods 
 li=[6,9,1,0,2]
 l2=[100,200,40]
@@ -28,5 +29,22 @@ newlist=[x  for x in l if "a" in x] # list connfrension
 print(newlist)
 li.sort(reverse=True)
 print(li)
-li3=list(reversed(li))
+li3=list(reversed(li)) # itratore method 
 print(li3)
+
+liii=li.copy()        # copy method 
+print("------",liii)
+
+li.append(50)
+print(li)
+print("------",liii)
+
+new_list=[45,[1,23],9]
+l_new=copy.deepcopy(new_list)
+print(l_new)
+
+
+t=(5,6,7,10,8,2)
+print(t[5])
+s={3,4,1,8}
+print(s)
